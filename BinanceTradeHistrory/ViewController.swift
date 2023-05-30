1201de6648597adbb572c6d76f62321c23123388
mwoo-git <hanulbom@gmail.com>
2023-05-30 19:19:17 +0900

    Initial Commit

---
+++ BinanceTradeHistrory/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BinanceTradeHistrory
+//
+//  Created by Mac on 2023/05/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
